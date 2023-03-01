b452e9b567bd1c80261a3b77d3d0e900eb559444
Mihran Marukyan <mihran.marukyan@ukg.com>
2023-03-01 21:17:21 +0400

    first commit

---
+++ src/main/java/com/ItComp/cicdproject/CicdProjectApplication.java
@@ -0,0 +1,21 @@
+package com.ItComp.cicdproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@SpringBootApplication
+@RestController
+public class CicdProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(CicdProjectApplication.class, args);
+    }
+
+    @GetMapping("/home")
+    public String homePage() {
+        return "Hello from Java!";
+    }
+
+}
---
+++ src/test/java/com/ItComp/cicdproject/CicdProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ItComp.cicdproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CicdProjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
